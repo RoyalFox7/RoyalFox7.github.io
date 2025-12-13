14afdbec18073858e6153d35624b1a5f68b3ee00
Datlac <62836501+Datlac@users.noreply.github.com>
2025-12-14 00:48:05 +0700

    Update lyrics.js

--- data/lyrics.js
+++ data/lyrics.js
@@ -3,9 +3,61 @@
 const lyricsDatabase = {
     // ID bài hát: `Lời bài hát`
     
-    
-
-   
+  
+   12: `
+[00:03.50] Những cơn sóng xô lâu đài vỡ trong bóng đêm
+[00:07.20] Nỗi đau cứ như đang vội ghé thăm trái tim
+[00:11.10] Chiều hoàng hôn kéo theo mây đen về
+[00:14.80] Phủ kín trong tâm tư bóng hình em
+[00:18.60] Ánh trăng dần khuất sau khu rừng
+[00:22.20] Chạy theo sau lưng anh ký ức không thể quên
+[00:25.80] Vì sau cơn mưa đêm qua
+[00:29.00] Không ai nhắc về chuyện đôi ta
+[00:30.80] Em vội vàng khóa con tim em trong một giây
+[00:34.50] Giọt sương nhẹ rớt theo cơn gió thu vội bay
+[00:38.20] Tình yêu chạy đến chân trời xa
+[00:41.80] Phút chốc đã khiến em gục ngã
+[00:46.50] Tựa lưng vào gốc cây chênh vênh em có thấy
+[00:50.00] Mây trời như cũng đã thành đôi
+[00:53.50] Thôi đành cất những ký ức bên nhau
+[00:57.00] Xem như quên đi mối tình đầu
+[01:00.50] Những cơn sóng xô lâu đài vỡ trong bóng đêm
+[01:04.20] Nỗi đau cứ như đang vội ghé thăm trái tim
+[01:08.10] Chiều hoàng hôn kéo theo mây đen về
+[01:11.80] Phủ kín trong tâm tư bóng hình em
+[01:15.60] Ánh trăng dần khuất sau khu rừng
+[01:19.20] Chạy theo sau lưng anh ký ức không thể quên
+[01:22.80] Vì sau cơn mưa đêm qua
+[01:25.80] Không ai nhắc về chuyện đôi ta
+[01:29.40]♪
+[01:58.00] Kết bộ phim em rời đi
+[02:00.50] Mang theo chút nắng tàn phai
+[02:03.00] Xõa làn tóc cúi mặt khóc
+[02:05.50] Đâu cần thiết biết chuyện mai
+[02:08.00] Đôi mắt đen nhòe dần
+[02:10.50] Cánh tay gạt vội nước mắt rơi
+[02:13.00] Soạn vài dòng tin nhắn
+[02:15.50] Rồi đốt đi khung hình
+[02:18.00] Dậm vào màu son mới em cất bước đi
+[02:22.00] Giữa phố đông người nhưng nỗi cô đơn vẫn cuộn chặt vào em
+[02:27.30] Những cơn sóng xô lâu đài vỡ trong bóng đêm
+[02:31.00] Nỗi đau cứ như đang vội ghé thăm trái tim
+[02:34.70] Chiều hoàng hôn kéo theo mây đen về
+[02:38.40] Phủ kín trong tâm tư bóng hình em
+[02:42.10] Ánh trăng dần khuất sau khu rừng
+[02:45.80] Chạy theo sau lưng anh ký ức không thể quên
+[02:49.50] Vì sau cơn mưa đêm qua
+[02:53.20] Không ai nhắc về chuyện đôi ta
+[02:56.60] Những cơn sóng xô lâu đài vỡ trong bóng đêm
+[03:00.30] Nỗi đau cứ như đang vội ghé thăm trái tim
+[03:04.00] Chiều hoàng hôn kéo theo mây đen về
+[03:07.70] Phủ kín trong tâm tư bóng hình em
+[03:11.40] Ánh trăng dần khuất sau khu rừng
+[03:15.10] Chạy theo sau lưng anh ký ức không thể quên
+[03:18.80] Vì sau cơn mưa đêm qua
+[03:22.50] Không ai nhắc về chuyện đôi ta
+`
+  
     
     
 };
